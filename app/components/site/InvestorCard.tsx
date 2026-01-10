ed27ac1dc7cb1e67f8a9cdce04a17e3e92f92bd4
Ahmed <ahmed.1196@buckeyemail.osu.edu>
2026-01-10 16:57:07 -0500

    investor card styling, favorites functionality implemented:

--- app/components/site/InvestorCard.tsx
+++ app/components/site/InvestorCard.tsx
@@ -20,78 +20,71 @@ export function InvestorCard({ investor }: Props) {
       <Card className="bg-sky-100/20 transition delay-20 hover:bg-sky-100/80 ">
         <CardHeader>
           <CardTitle>
-            <div className="relative flex-w-full justify-between">
-              {/* Avatar, Name, Type Container */}
-              <div className="flex items-center w-max gap-4 pt-1 ">
-                {/* Avatar */}
-                <div className="flex items-center">
-                  {investor.avatarUrl ? (
-                    <img
-                      src={investor.avatarUrl}
-                      alt={investor.name}
-                      className="h-9 w-9 rounded-full object-cover"
-                    />
-                  ) : (
-                    <div className="h-9 w-9 rounded-full bg-gray-300 flex items-center justify-center text-sm font-medium text-gray-700">
-                      {investor.name
-                        .split(" ")
-                        .map((n) => n[0])
-                        .join("")}
-                    </div>
-                  )}
+            {/* Avatar, Name, Type, Star Container */}
+            <div>
+              <div className="flex w-full justify-between items-start">
+                {/* Avatar, Name, Type Container */}
+                <div className="flex items-center w-max gap-4 pt-1 ">
+                  {/* Avatar */}
+                  <div className="flex items-center">
+                    {investor.avatarUrl ? (
+                      <img
+                        src={investor.avatarUrl}
+                        alt={investor.name}
+                        className="h-9 w-9 rounded-full object-cover"
+                      />
+                    ) : (
+                      <div className="h-9 w-9 rounded-full bg-gray-300 flex items-center justify-center text-sm font-medium text-gray-700">
+                        {investor.name
+                          .split(" ")
+                          .map((n) => n[0])
+                          .join("")}
+                      </div>
+                    )}
+                  </div>
+                  {/* Name and Type */}
+                  <div className="flex flex-col ">
+                    <Link href={`/investor-profile/${investor.id}`}>
+                      <span className="text-lg font-medium text-gray-900 hover:underline cursor-pointer">
+                        {investor.name}
+                      </span>
+                    </Link>
+                    <div className="text-xs text-gray-600">{investor.type}</div>
+                  </div>
                 </div>
-                {/* Name and Type */}
-                <div className="flex flex-col gap-1">
-                  <Link href={`/investor-profile/${investor.id}`}>
-                    <span className="font-medium text-gray-900 hover:underline cursor-pointer">
-                      {investor.name}
-                    </span>
-                  </Link>
-                  <div className="text-xs text-gray-600">{investor.type}</div>
+                <div className="ml-auto">
+                  <FavoriteStar
+                    investorId={investor.id}
+                    initiallyFavorited={investor.isFavorited}
+                  />
                 </div>
               </div>
-              {/* Stat Container */}
-              <div
-                className="
-    my-4
-    grid grid-cols-2 gap-x-6 gap-y-3
-    text-right
-    sm:grid-cols-2 sm:text-right sm:py-1 sm:gap-y-6
-    md:grid-cols-2 md:gap-6 md:ml-auto md:pr-10 md:text-right
-    lg:flex lg:gap-x-12
-  "
-              >
-                <Stat
-                  label="Avg. Check"
-                  value={
-                    investor.averageInvestmentSize
-                      ? `$${investor.averageInvestmentSize.toLocaleString()}`
-                      : "—"
-                  }
-                />
-                <Stat
-                  label="Latest Inv. Company"
-                  value={investor.mostRecentInvestmentCompany ?? "Undisclosed"}
-                />
-                <Stat
-                  label="Latest Inv. Date"
-                  value={
-                    investor.mostRecentInvestmentDate
-                      ? investor.mostRecentInvestmentDate.toLocaleDateString()
-                      : "—"
-                  }
-                />
-                <Stat
-                  label="Total Inv."
-                  value={investor.totalInvestments.toString()}
-                />
-              </div>
-              <div className="absolute top-2 right-2">
-                <FavoriteStar
-                  investorId={investor.id}
-                  initiallyFavorited={investor.isFavorited}
-                />
-              </div>
+            </div>
+            <div className="grid grid-cols-4 gap-x-12 max-w-container mx-40 pt-8 pb-4">
+              <Stat
+                label="Avg. Check"
+                value={
+                  investor.averageInvestmentSize
+                    ? `$${investor.averageInvestmentSize.toLocaleString()}`
+                    : "—"
+                }
+              />
+              <Stat
+                label="Latest Inv. Company"
+                value={investor.mostRecentInvestmentCompany ?? "Undisclosed"}
+              />
+              <Stat
+                label="Latest Inv. Date"
+                value={
+                  investor.mostRecentInvestmentDate
+                    ? investor.mostRecentInvestmentDate.toLocaleDateString()
+                    : "—"
+                }
+              />
+              <Stat
+                label="Total Inv."
+                value={investor.totalInvestments.toString()}
+              />
             </div>
           </CardTitle>
         </CardHeader>
@@ -105,12 +98,20 @@ export function InvestorCard({ investor }: Props) {
 function Stat({ label, value }: { label: string; value: string }) {
   return (
     <div className="flex flex-col text-right min-w-0">
-      <span className="text-xs text-gray-500">{label}</span>
-      {label.toLocaleLowerCase() === "avg. check" ? (
-        <span className="font-medium text-green-600">{value}</span>
-      ) : (
-        <span className="font-medium text-black">{value}</span>
-      )}
+      <span className="text-xs text-gray-500 tracking-light tabular-nums">
+        {label}
+      </span>
+      <div className="text-xl">
+        {label.toLocaleLowerCase() === "avg. check" ? (
+          <span className="text-green-600 tracking-light tabular-nums">
+            {value}
+          </span>
+        ) : (
+          <span className="text-black tracking-light tabular-nums">
+            {value}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
