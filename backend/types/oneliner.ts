8f25115186fe99636fb92012add41590b6a56eef
Ahmed <ahmed.1196@buckeyemail.osu.edu>
2026-05-12 12:23:28 -0400

    created Types for oneliner interactions between user and LLM, and reflected on Prisma Schema

---
+++ backend/types/oneliner.ts
@@ -0,0 +1,33 @@
+export type OneLinerSubmission = {
+    id: string; 
+    target: "Businesses" | "Consumers" | "Both";
+    industry :string;
+    name: string;
+    explanation: string;
+    user: string;
+    problem: string;
+    result: string;
+    unique: string;
+}
+
+export type GeneratedOneLiner = {
+    id: number;
+    response: string;
+}
+
+// array of responses from LLM
+// generated_response = [{id = 0, response = "asdsada"}, {id = 1, response= "sdfsdf"}, etc]
+export type OneLinerResponse = {
+    generated_responses: GeneratedOneLiner[];
+}
+
+export type OneLinerInteraction = {
+    id: string;
+    submission: OneLinerSubmission;
+    response: OneLinerResponse;
+}
+
+
+export type OneLinerHistory = {
+    interactions: OneLinerInteraction[];
+}
