33b0f8e6ae57eac170f20c4727ab30d79c58eb39
Ahmed <ahmed.1196@buckeyemail.osu.edu>
2026-05-25 13:24:59 -0400

    added components to display historical one liners

--- app/components/site/one-liners/DisplayHistoricalOneLiners.tsx
+++ app/components/site/one-liners/DisplayHistoricalOneLiners.tsx
@@ -1,6 +1,18 @@
+"use client";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/app/components/ui/tabs"
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input"
+import { Label } from "@/app/components/ui/label"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/components/ui/popover"
 
-
+// take a look at tabs and popover documentation, need to 
+// figure out what this page should look like.
+// to have delete one liner functionality- must have a parent server component which contains this.
 export function DisplayHistoricalOneLiners() {
     return (
         <div>Hi</div>
